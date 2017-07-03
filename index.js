6d0e9c07c9043858c4e34b85d50ba8678c48d66a
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-03 21:30:14 +0000

    got it to print the contributers

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
-var request = require("request")
-var repoOwner = process.argv.slice(2)[0]
-var repo = process.argv.slice(3)
-var url = `https://api.github.com/${repoOwner}/${repo}/graphs/contributors`
 
-request.get(url).pipe(fs.createWriteStream('./avatars/future.jpeg'))
+// Libraries
+var request = require("request");
+
+// Define console input as variables
+var repoOwner = process.argv.slice(2)[0];
+var repo = process.argv.slice(2)[1];
+
+// Set the needed token and user, and compute api string.
+var GITHUB_USER = "Sputn1kolas";
+var GITHUB_TOKEN = "313a8989312b8c74d35a0a104a30c4f3d0aeea03";
+var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repo}/contributors`;
+
+var options = {
+  url: url,
+  headers: {
+    'User-Agent':GITHUB_USER
+  }
+}
+
+console.log(url)
+function getContributors() {
+  request.get(options, function(err, response, body){
+    if(err) {
+      console.log("there is an error:", err);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      var json = JSON.parse(body);
+       for(var i =0; i < json.length; i++){
+        var contributor = json[i]['login']
+        var contributorAvatar = json[i]['avatar_url']
+
+        console.log("contributer: ", contributor)
+      }
+    }
+  })
+}
+//.pipe(fs.createWriteStream('./contributers.js'));
+
+getContributors()
 
 
-//
+// function getRepoContributors(repoOwner, repoName, cb) {
+
+// }
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
