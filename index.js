c83748335efaef1d00f5561403be8a203d2212a6
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-03 22:32:42 +0000

    now validates command line inputs, and is nested within a function. Doing this intially caused hoisting problems

--- index.js
+++ index.js
@@ -5,46 +5,58 @@ var fs = require("fs");
 
 
 // Define console input as variables
-var repoOwner = process.argv.slice(2)[0];
-var repo = process.argv.slice(2)[1];
-
-// Set the needed token and user, and compute api string.
-var GITHUB_USER = "Sputn1kolas";
-var GITHUB_TOKEN = "313a8989312b8c74d35a0a104a30c4f3d0aeea03";
-var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repo}/contributors`;
-
-var options = {
-  url: url,
-  headers: {
-    'User-Agent':GITHUB_USER
+function consoleInputs() {
+  if(!process.argv.slice(2)[0] || !process.argv.slice(2)[1]) {
+   console.log("ERROR. Please enter in the style \n node index.js repoOwner repo")
+   return;
   }
-}
 
-function saveAvatars(json) {
-  for(var i =0; i < json.length; i++){
-    var contributor = json[i]['login']
-    var contributorAvatarURL = json[i]['avatar_url']
-    downloadImagebyURL(contributorAvatarURL, contributor)
-  }
-}
-
-function downloadImagebyURL(url, contributor) {
-  request.get(url).pipe(fs.createWriteStream(`./avatar/${contributor}.jpeg`))
-}
+  // Set the needed token and user, and compute api string.
+  var GITHUB_USER = "Sputn1kolas";
+  var GITHUB_TOKEN = "313a8989312b8c74d35a0a104a30c4f3d0aeea03";
 
 
-function getAvatars() {
-  request.get(options, function(err, response, body){
-    if(err) {
-      console.log("there is an error:", err);
-      return;
+  var repoOwner = process.argv.slice(2)[0]
+  var repo = process.argv.slice(2)[1];
+  var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repo}/contributors`;
+
+
+
+  options = {
+    url: url,
+    headers: {
+      'User-Agent':GITHUB_USER
     }
-    if (response.statusCode === 200) {
-      var json = JSON.parse(body);
-      saveAvatars(json);
+  }
+
+  function getAvatars() {
+    request.get(options, function(err, response, body){
+      if(err) {
+        console.log("there is an error:", err);
+        return;
       }
-  })
+      if (response.statusCode === 200) {
+        var json = JSON.parse(body);
+        saveAvatars(json);
+        }
+    })
+  }
+
+  function saveAvatars(json) {
+    for(var i =0; i < json.length; i++){
+      var contributor = json[i]['login']
+      var contributorAvatarURL = json[i]['avatar_url']
+      downloadImagebyURL(contributorAvatarURL, contributor)
+    }
+  }
+
+  function downloadImagebyURL(url, contributor) {
+    request.get(url).pipe(fs.createWriteStream(`./avatar/${contributor}.jpeg`))
+  }
+  getAvatars()
 }
 
-getAvatars()
+consoleInputs()
+
+
 
