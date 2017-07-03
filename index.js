f3c03705a14759a266c9d6547ba596fbed61cd08
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-03 22:53:27 +0000

    now keeps keys secret

--- index.js
+++ index.js
@@ -3,20 +3,24 @@
 var request = require("request");
 var fs = require("fs");
 
+var dotenv = require('dotenv').config();
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN =  process.env.GITHUB_TOKEN;
+
+// console.log(GITHUB_USER)
 
 // Define console input as variables
 function consoleInputs() {
   if(!process.argv.slice(2)[0] || !process.argv.slice(2)[1]) {
-   console.log("ERROR. Please enter in the style \n node index.js repoOwner repo")
+   console.log("ERROR. Please enter in the style \n node index.js repoOwner repo");
    return;
   }
 
   // Set the needed token and user, and compute api string.
-  var GITHUB_USER = "Sputn1kolas";
-  var GITHUB_TOKEN = "313a8989312b8c74d35a0a104a30c4f3d0aeea03";
 
 
-  var repoOwner = process.argv.slice(2)[0]
+
+  var repoOwner = process.argv.slice(2)[0];
   var repo = process.argv.slice(2)[1];
   var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repo}/contributors`;
 
@@ -44,19 +48,19 @@ function consoleInputs() {
 
   function saveAvatars(json) {
     for(var i =0; i < json.length; i++){
-      var contributor = json[i]['login']
-      var contributorAvatarURL = json[i]['avatar_url']
-      downloadImagebyURL(contributorAvatarURL, contributor)
+      var contributor = json[i]['login'];
+      var contributorAvatarURL = json[i]['avatar_url'];
+      downloadImagebyURL(contributorAvatarURL, contributor);
     }
   }
 
   function downloadImagebyURL(url, contributor) {
-    request.get(url).pipe(fs.createWriteStream(`./avatar/${contributor}.jpeg`))
+    request.get(url).pipe(fs.createWriteStream(`./avatar/${contributor}.jpeg`));
   }
-  getAvatars()
+  getAvatars();
 }
 
-consoleInputs()
+consoleInputs();
 
 
 
